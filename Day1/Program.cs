7bd88ae30a84cc5dc0a4ffa207ffc13ca848ef06
9102 <s.maier@oth-aw.de>
2023-12-01 16:10:45 +0100

    feat(Day1): Solution for Day1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,13 +1,20 @@
 using CommonFunctions;
 using System;
+using System.Text.RegularExpressions;
 
 class Program{
   static void Main()
   {
-    var input = InputReader.ReadInData(InputType.Example);
+    var input = InputReader.ReadInData(InputType.Full);
+    int calibrationValueSum = 0;
     foreach(var line in input)
     {
-      Console.WriteLine(line);
+      string numberSearch = "[0-9]";
+      var matches = Regex.Matches(line,numberSearch);
+      string calibrationValue = matches[0].Value + matches[matches.Count-1].Value;
+      Console.WriteLine($"The calibrationvalue is {calibrationValue}");
+      calibrationValueSum += int.Parse(calibrationValue);
     }
+    Console.WriteLine($"The sum of all the calibration values is {calibrationValueSum}");
   }
 }
