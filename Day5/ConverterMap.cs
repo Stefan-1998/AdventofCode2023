336f3b2c6d7c824b562c4fa1dcd76a515e78ddbb
9102 <s.maier@oth-aw.de>
2023-12-07 20:32:45 +0100

    feat(Day5): Got Solution from the internet

--- Day5/ConverterMap.cs
+++ Day5/ConverterMap.cs
@@ -24,5 +24,43 @@ namespace Day5{
             }
             return seed;
         }
+        public List<(long StartPoint, long Range )> ConvertRanges(long seedStartingPoint, long seedRange )
+        {
+            //first splitting up and then converting could help
+            var convertedSections = new List<(long StartPoint, long Range)>();
+            var notConvertedSections = new Queue<(long StartPoint, long Range)>();
+            notConvertedSections.Enqueue((StartPoint: seedStartingPoint, Range: seedRange));
+            foreach(var conversionRule in ConversionRules)
+            {
+                //cases no Conversion needed
+                if(seedStartingPoint > conversionRule.SourceStartPoint+conversionRule.Range-1){continue;}
+                if(seedStartingPoint+seedRange-1 < conversionRule.SourceStartPoint){continue;}
+
+                //case converter on the left
+                if(conversionRule.SourceStartPoint < seedStartingPoint && conversionRule.SourceStartPoint+conversionRule.Range-1 > seedStartingPoint)
+                {
+                    var offset= conversionRule.Range - (seedStartingPoint-conversionRule.SourceStartPoint);
+                    convertedSections.Add((StartPoint: seedStartingPoint+conversionRule.DestinationStartPoint-conversionRule.SourceStartPoint, Range:offset));
+                    seedStartingPoint = seedStartingPoint+ offset;
+                    seedRange = seedRange - offset;
+                    continue;
+                }
+                //case converter on the right
+                if(conversionRule.SourceStartPoint < (seedStartingPoint+seedRange-1) && (conversionRule.SourceStartPoint+conversionRule.Range-1) >(seedStartingPoint+seedRange-1))
+                {
+                    var offset= seedStartingPoint + seedRange -conversionRule.SourceStartPoint;
+                    convertedSections.Add((StartPoint: conversionRule.DestinationStartPoint, Range:offset));
+                    seedRange = seedRange - offset;
+                    continue;
+                }
+                //case converter in the middle
+                if(conversionRule.SourceStartPoint > seedStartingPoint && (conversionRule.SourceStartPoint + conversionRule.Range-1) < seedStartingPoint + seedRange -1)
+                {
+                    convertedSections.Add((StartPoint: conversionRule.DestinationStartPoint, Range:conversionRule.Range));
+                    continue;
+                }
+            }
+            return convertedSections;
+        }
     }
 }
--- Day5/Program.cs
+++ Day5/Program.cs
@@ -6,12 +6,13 @@ using Day5;
 class Program{
   static void Main()
   {
-    var input = InputReader.ReadInData(InputType.Full).ToList();
+    var input = InputReader.ReadInData(InputType.Example).ToList();
     foreach(var line in input)
     {
       Console.WriteLine(line);
     }
     var seeds = GetSeeds(input[0]);
+    var seedRanges = GetSeedRanges(input[0]);
     input.RemoveAt(0);
     input.RemoveAt(0);
 
@@ -29,6 +30,25 @@ class Program{
     }
     var lowestLocationNumber = locations.Min();
     Console.WriteLine($"The lowest location is {lowestLocationNumber}");
+
+    //puzzle 2
+    /*var results = new List<(long StartPoint, long Range)>();
+    foreach((long StartPoint, long Range) seedRange in seedRanges)
+    {
+      var convertedSeedRanges = new List<(long StartPoint, long Range)>(){seedRange};
+      foreach(var converter in converters)
+      {
+
+        //var normalList = converter.GetNotConvertedRanges(seedRange.StartPoint, seedRange.Range);
+        var convertedList = converter.ConvertRanges(seedRange.StartPoint, seedRange.Range);
+        convertedSeedRanges = normalList.Concat(convertedList).ToList();
+      }
+      results = results.Concat(convertedSeedRanges).ToList();
+
+    }
+    var minValue = results.Min(t => t.StartPoint);
+    Console.WriteLine($"The minimum is {minValue}");*/
+    Solution2();
   }
 
   static List<long> GetSeeds(string input)
@@ -44,4 +64,102 @@ class Program{
     }
     return seeds;
   }
+  static List<(long StartPoint,long Range)> GetSeedRanges(string input)
+  {
+    var seedsLine = input.Split(':')[1];
+    var digitRegex = new Regex(@"\d+");
+    var matches = digitRegex.Matches(seedsLine);
+
+    var seedRanges = new List<(long StartPoint, long Range)>();
+    for(int i=0; i< matches.Count();i=i+2)
+    {
+      seedRanges.Add((StartPoint: long.Parse(matches[i].Value), Range: long.Parse(matches[i+1].Value)));
+    }
+    return seedRanges;
+
+  }
+  static void Solution2()
+        {
+            string[] lines = File.ReadAllLines(@"./Input.txt");
+
+            List<(long, long)> seedRanges = new();
+            var seedsRangesInput = lines[0].Split().Where(x => x != string.Empty && x != "seeds:").Select(x => long.Parse(x)).ToList();
+            for (int i = 0; i < seedsRangesInput.Count; i+=2)
+            {
+                seedRanges.Add((seedsRangesInput[i], seedsRangesInput[i + 1]));
+            }
+
+            List<(long, long)> seedRangesChanged = new();
+            List<(long, long)> seedRangesTemp = new();
+
+            foreach (var line in lines[1..])
+            {
+                seedRangesTemp = new();
+                if (line == "" || line.Contains("map"))
+                {
+                    foreach(var seedRange in seedRangesChanged)
+                        seedRanges.Add((seedRange.Item1, seedRange.Item2));
+                    seedRangesChanged = new();
+                    continue;
+                }
+
+                var map = line.Split().Where(x => x != string.Empty).Select(x => long.Parse(x)).ToList();
+                foreach (var seedRange in seedRanges)
+                {
+                    var rangeStart = map[1];
+                    var rangeStop = map[1] + map[2] - 1;
+                    var shift = map[0] - map[1];
+
+                    var seedStart = seedRange.Item1;
+                    var seedStop = seedRange.Item1 + seedRange.Item2 - 1;
+
+                    long commonRangeStart = 0;
+                    long commonRangeStop = 0;
+
+                    if (rangeStart < seedStart && rangeStop >= seedStart & rangeStop <= seedStop)
+                    {
+                        commonRangeStart = seedStart;
+                        commonRangeStop = rangeStop;
+                        seedRangesChanged.Add((commonRangeStart + shift, commonRangeStop - commonRangeStart + 1));
+                        if (commonRangeStop + 1 <= seedStop)
+                            seedRangesTemp.Add((commonRangeStop + 1, seedStop - (commonRangeStop + 1) + 1));
+                    }
+                    else if (rangeStart >= seedStart && rangeStop <= seedStop)
+                    {
+                        commonRangeStart = rangeStart;
+                        commonRangeStop = rangeStop;
+                        seedRangesChanged.Add((commonRangeStart + shift, commonRangeStop - commonRangeStart + 1));
+                        if (commonRangeStop + 1 <= seedStop)
+                            seedRangesTemp.Add((commonRangeStop + 1, seedStop - (commonRangeStop + 1) + 1));
+                        if (commonRangeStart - 1 >= seedStart)
+                            seedRangesTemp.Add((seedStart, commonRangeStart - 1 - (seedStart) + 1));
+                    }
+                    else if(rangeStart >= seedStart && rangeStart <= seedStop && rangeStop > seedStop)
+                    {
+                        commonRangeStart = rangeStart;
+                        commonRangeStop = seedStop;
+                        seedRangesChanged.Add((commonRangeStart + shift, commonRangeStop - commonRangeStart + 1));
+                        if (commonRangeStart - 1 >= seedStart)
+                            seedRangesTemp.Add((seedStart, commonRangeStart - 1 - (seedStart) + 1));
+                    }
+                    else if (rangeStart < seedStart && rangeStop > seedStop)
+                    {
+                        commonRangeStart = seedStart;
+                        commonRangeStop = seedStop;
+                        seedRangesChanged.Add((commonRangeStart + shift, commonRangeStop - commonRangeStart + 1));
+                    }
+                    else
+                    {
+                        seedRangesTemp.Add((seedStart, seedStop - seedStart + 1));
+                    }
+                }
+
+                seedRanges = seedRangesTemp;
+            }
+
+            foreach (var seedRange in seedRangesChanged)
+                seedRanges.Add((seedRange.Item1, seedRange.Item2));
+
+            Console.WriteLine(seedRanges.Min(x => x.Item1));
+        }
 }
