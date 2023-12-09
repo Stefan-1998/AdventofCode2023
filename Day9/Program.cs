b5a148444ce72db7c1fcc86a30aff926ba1bae51
9102 <s.maier@oth-aw.de>
2023-12-09 22:22:46 +0100

    feat(Day9): Solution for second puzzle

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -20,6 +20,16 @@ class Program{
 
     }
     Console.WriteLine($"The summary of all the extra polated values is {result}");
+
+    result=0;
+    foreach(var line in input)
+    {
+      var digitRegex = new Regex(@"[-]?\d+");
+      var sequence = digitRegex.Matches(line).Select(x => long.Parse(x.Value)).ToList();
+      var extraPolateValue = ExtraPolateAtBeginning(sequence);
+      result += extraPolateValue;
+    }
+    Console.WriteLine($"The summary of all the extra polated values at the beginning is {result}");
   }
   private static long ExtraPolate(List<long> sequence)
   {
@@ -32,5 +42,17 @@ class Program{
     }
     var extraPolate = ExtraPolate(substractedSequence);
     return sequence[sequence.Count()-1]+extraPolate; 
+  }
+  private static long ExtraPolateAtBeginning(List<long> sequence)
+  {
+    if(sequence.All(x=>x==0))
+      return 0;
+    var substractedSequence = new List<long>();
+    for(int i = 0; i< sequence.Count()-1; i++)
+    {
+      substractedSequence.Add(sequence[i+1]-sequence[i]);
+    }
+    var extraPolate = ExtraPolateAtBeginning(substractedSequence);
+    return sequence[0]-extraPolate; 
   }
 }
